a0582c6cf29e67faeea984034d554ee8d1eb7653
Tadeusz Cekala <eltadziko@gmail.com>
2012-10-14 23:27:46 +0200

    Poprawienie modelu i admina

--- Dentist/urls.py
+++ Dentist/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from Dentist import views
-from django.contrib import admin
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # (r'^hello/$', views.hello),
