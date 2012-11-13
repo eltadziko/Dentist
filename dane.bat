f8950ff4cbf58c35bf23ff33491689bed7f3c72c
Tadeusz Cekala <eltadziko@gmail.com>
2012-11-13 20:21:33 +0100

    Poprawki fixtures

--- dane.bat
+++ dane.bat
@@ -3,3 +3,10 @@ manage.py loaddata group.yaml
 manage.py loaddata dental_office.yaml
 manage.py loaddata tooth.yaml
 manage.py loaddata insurance.yaml
+manage.py loaddata appointment_type.yaml
+manage.py loaddata user.yaml
+manage.py loaddata patient.yaml
+manage.py loaddata patient_diseases.yaml
+manage.py loaddata dentist.yaml
+manage.py loaddata password_creation.yaml
+manage.py loaddata hours.yaml
