ad23a3a1172e3607b30834cdca4f8370216098b0
Tadeusz Cekala <eltadziko@gmail.com>
2012-12-09 20:10:01 +0100

    Poprawki danych

--- dane.bat
+++ dane.bat
@@ -9,9 +9,5 @@ manage.py loaddata patient.yaml
 manage.py loaddata patient_diseases.yaml
 manage.py loaddata dentist.yaml
 manage.py loaddata password_creation.yaml
-manage.py loaddata hours.yaml
-manage.py loaddata dates.yaml
-manage.py loaddata appointment.yaml
 manage.py loaddata tooth_part.yaml
 manage.py loaddata loss_type.yaml
-manage.py loaddata tooth_loss.yaml
